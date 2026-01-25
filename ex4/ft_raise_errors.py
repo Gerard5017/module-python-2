2696c9935c76972cfb7c151c53dfa2580b469c5a
Gerard5017 <1563poi@gmail.com>
2026-01-25 23:55:11 +0000

    k

--- ex4/ft_raise_errors.py
+++ ex4/ft_raise_errors.py
@@ -20,13 +20,13 @@ def check_plant_health(plant_name: str, water_level: int,
     """
     if not plant_name or plant_name == "":
         raise ValueError("Plant name cannot be empty!")
-    if water_level <= 1:
+    if water_level < 1:
         raise ValueError(f"Water level {water_level} is too low min 1")
-    if water_level >= 10:
+    if water_level > 10:
         raise ValueError(f"Water level {water_level} is too high max 10")
-    if sunlight_hours <= 2:
+    if sunlight_hours < 2:
         raise ValueError(f"Sunlight hours {sunlight_hours} is too low min 2")
-    if sunlight_hours >= 12:
+    if sunlight_hours > 12:
         raise ValueError(
             f"Sunlight hours {sunlight_hours} is too high max 12"
         )
